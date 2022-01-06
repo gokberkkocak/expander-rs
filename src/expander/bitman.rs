54dfe0295c1a7d622e9b36e4840f3ccda8691cf0
Gökberk KOÇAK <gk34@st-andrews.ac.uk>
2022-01-06 17:12:41 +0000

    Add the ability choose different hashers.

--- src/expander/bitman.rs
+++ src/expander/bitman.rs
@@ -1,24 +1,31 @@
-use fnv::FnvHashSet;
-
 use crate::{expander::Expander, JsonSet};
 
 fn convert_itemset(sol: &[u8]) -> u128 {
-    let mut sol_u64 = 0;
+    let mut sol_u128 = 0;
     for i in sol {
-        sol_u64 |= 1 << ((i - 1) as u64);
+        sol_u128 |= 1 << (i - 1);
     }
-    sol_u64
+    sol_u128
 }
 
-pub(crate) struct BitManipulatorExpander;
+pub(crate) struct BitManipulatorExpander<T> {
+    _phantom: std::marker::PhantomData<T>,
+}
 
-impl Expander for BitManipulatorExpander {
+impl<T> Expander<T> for BitManipulatorExpander<T>
+where
+    T: Default,
+    T: IntoIterator,
+    T::Item: Into<u128>,
+    T: crate::expander::Insert<u128>,
+    T: crate::expander::Contains<u128>,
+{
     type SolutionType = u128;
 
     type HashType = u128;
 
-    fn expand(parsed_set: Vec<JsonSet>) -> FnvHashSet<Self::HashType> {
-        let mut final_set = FnvHashSet::default();
+    fn expand(parsed_set: Vec<JsonSet>) -> T {
+        let mut final_set = T::default();
         let parsed_set = parsed_set
             .iter()
             .map(|x| convert_itemset(&x.set))
@@ -29,48 +36,130 @@ impl Expander for BitManipulatorExpander {
         final_set
     }
 
-    fn expand_one_solution_to_lower_level(
-        solution: &mut Self::SolutionType,
-        final_set: &mut FnvHashSet<Self::HashType>,
-    ) {
+    fn expand_one_solution_to_lower_level(solution: &mut Self::SolutionType, final_set: &mut T) {
         let length = solution.count_ones();
         if length > 1 {
             for i in 0..u128::BITS {
                 if (*solution & (1 << i)) >> i == 1 {
                     let mut new_sol = *solution ^ (1 << i);
-                    if !final_set.contains(&new_sol) {
+                    if !final_set.contains_(&new_sol) {
                         Self::expand_one_solution_to_lower_level(&mut new_sol, final_set);
                     }
                 }
             }
         }
-        final_set.insert(*solution);
+        final_set.insert_(*solution);
     }
-
-
 }
 
 #[cfg(test)]
 mod tests {
 
+    use std::collections::HashSet;
+
+    use ahash::AHashSet;
+    use fnv::FnvHashSet;
+    use fxhash::FxHashSet;
+
     use super::*;
     #[test]
-    fn test_1() {
+    fn test_1_fnv() {
         let parsed_set = vec![
             JsonSet { set: vec![1, 2, 3] },
             JsonSet { set: vec![4, 5, 6] },
         ];
-        assert_eq!(BitManipulatorExpander::expand(parsed_set).len(), 14);
+        assert_eq!(
+            BitManipulatorExpander::<FnvHashSet<u128>>::expand(parsed_set).len(),
+            14
+        );
     }
 
     #[test]
-    fn test_2() {
+    fn test_2_fnv() {
         let parsed_set = vec![
             JsonSet {
                 set: vec![57, 58, 59, 60],
             },
             JsonSet { set: vec![60, 99] },
         ];
-        assert_eq!(BitManipulatorExpander::expand(parsed_set).len(), 17);
+        assert_eq!(
+            BitManipulatorExpander::<FnvHashSet<u128>>::expand(parsed_set).len(),
+            17
+        );
+    }
+
+    #[test]
+    fn test_1_fx() {
+        let parsed_set = vec![
+            JsonSet { set: vec![1, 2, 3] },
+            JsonSet { set: vec![4, 5, 6] },
+        ];
+        assert_eq!(
+            BitManipulatorExpander::<FxHashSet<u128>>::expand(parsed_set).len(),
+            14
+        );
+    }
+    #[test]
+    fn test_2_fx() {
+        let parsed_set = vec![
+            JsonSet {
+                set: vec![57, 58, 59, 60],
+            },
+            JsonSet { set: vec![60, 99] },
+        ];
+        assert_eq!(
+            BitManipulatorExpander::<FxHashSet<u128>>::expand(parsed_set).len(),
+            17
+        );
+    }
+
+    #[test]
+    fn test_1_std() {
+        let parsed_set = vec![
+            JsonSet { set: vec![1, 2, 3] },
+            JsonSet { set: vec![4, 5, 6] },
+        ];
+        assert_eq!(
+            BitManipulatorExpander::<HashSet<u128>>::expand(parsed_set).len(),
+            14
+        );
+    }
+    #[test]
+    fn test_2_std() {
+        let parsed_set = vec![
+            JsonSet {
+                set: vec![57, 58, 59, 60],
+            },
+            JsonSet { set: vec![60, 99] },
+        ];
+        assert_eq!(
+            BitManipulatorExpander::<HashSet<u128>>::expand(parsed_set).len(),
+            17
+        );
+    }
+
+    #[test]
+    fn test_1_ahash() {
+        let parsed_set = vec![
+            JsonSet { set: vec![1, 2, 3] },
+            JsonSet { set: vec![4, 5, 6] },
+        ];
+        assert_eq!(
+            BitManipulatorExpander::<AHashSet<u128>>::expand(parsed_set).len(),
+            14
+        );
+    }
+    #[test]
+    fn test_2_ahash() {
+        let parsed_set = vec![
+            JsonSet {
+                set: vec![57, 58, 59, 60],
+            },
+            JsonSet { set: vec![60, 99] },
+        ];
+        assert_eq!(
+            BitManipulatorExpander::<AHashSet<u128>>::expand(parsed_set).len(),
+            17
+        );
     }
 }
--- src/expander/mod.rs
+++ src/expander/mod.rs
@@ -2,15 +2,72 @@ pub(crate) mod bitman;
 pub(crate) mod bitvec;
 pub(crate) mod hashonly;
 
-use fnv::FnvHashSet;
-use crate::JsonSet;
+use std::{collections::HashSet, hash::Hash};
 
-pub(crate) trait Expander {
+use ahash::AHashSet;
+use fnv::FnvHashSet;
+use fxhash::FxHashSet;
+
+use crate::JsonSet;
+pub(crate) trait Expander<T>
+where
+    Self::HashType: Eq,
+    Self::HashType: Hash,
+    T: Default,
+    T: IntoIterator,
+    T::Item: Into<Self::HashType>,
+    T: Insert<Self::HashType>,
+    T: Contains<Self::HashType>,
+{
     type SolutionType;
     type HashType;
-    fn expand(parsed_set: Vec<JsonSet>) -> FnvHashSet<Self::HashType>;
-    fn expand_one_solution_to_lower_level(
-        solution: &mut Self::SolutionType,
-        final_set: &mut FnvHashSet<Self::HashType>,
-    );
+    fn expand(parsed_set: Vec<JsonSet>) -> T;
+    fn expand_one_solution_to_lower_level(solution: &mut Self::SolutionType, final_set: &mut T);
 }
+
+pub(crate) trait Insert<T>
+where
+    T: Eq,
+    T: Hash,
+{
+    fn insert_(&mut self, item: T);
+}
+
+pub(crate) trait Contains<T>
+where
+    T: Eq,
+    T: Hash,
+{
+    fn contains_(&self, item: &T) -> bool;
+}
+
+macro_rules! impl_insert_and_contains {
+    ($t:ident) => {
+        impl<T> Insert<T> for $t<T>
+        where
+            T: Eq,
+            T: Hash,
+        {
+            #[inline]
+            fn insert_(&mut self, item: T) {
+                self.insert(item);
+            }
+        }
+
+        impl<T> Contains<T> for $t<T>
+        where
+            T: Eq,
+            T: Hash,
+        {
+            #[inline]
+            fn contains_(&self, item: &T) -> bool {
+                self.contains(item)
+            }
+        }
+    };
+}
+
+impl_insert_and_contains!(HashSet);
+impl_insert_and_contains!(FxHashSet);
+impl_insert_and_contains!(FnvHashSet);
+impl_insert_and_contains!(AHashSet);
